c15aeb63ef9643a7bd62fcd481fd65c3354c11ca
bajaj29 <rihen.bajaj.cer14@itbhu.ac.in>
2020-07-24 22:18:32 +0530

    create app commit

---
+++ covid-19/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Test from './component/test/Test'
+
+function App() {
+	return (
+		<React.Fragment>
+					<Test />
+		</React.Fragment >
+	)
+}
+
+export default App
